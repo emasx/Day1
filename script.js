c53a4dad9131f4a9a3a610c23fac4c7ee5436e10
emasx <29961897+emasx@users.noreply.github.com>
2022-08-14 23:35:03 +0300

    Day1

---
+++ script.js
@@ -0,0 +1,28 @@
+// A variable = is a box in which we can store a value.
+// .Conventions and .Rules for naming variables
+// camelCase = the first word with a LOWERCASE and then all the next words with UPPERCASE
+// new, function... are reserved keywords in JavaScript   
+let firstName = "De";
+let lastName  = "Emas";
+
+let myFirstJob = "teacher";
+let myCurrentJob = "Web Designer";
+
+
+console.log(firstName);
+console.log(lastName);
+console.log(myFirstJob);
+console.log(myCurrentJob);
+
+
+// 1.Declare variables called 'country', 'continent' and 'population' and assign their values 
+//   according to your own country.
+// 2.Log their values to the console
+
+let country = "Romania";
+let continent = "Europe";
+let population = 19;
+
+console.log(country);
+console.log(continent);
+console.log(population);
